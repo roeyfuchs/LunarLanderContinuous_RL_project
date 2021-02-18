6492d6825e9aec9c7374b301da891bf9fd8a6168
RoeyFuchs <fuchsroey@gmail.com>
2021-02-18 21:26:40 +0200

    call to train

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ import gym
 import table_method
 
 env = gym.make("LunarLanderContinuous-v2")
-observation = env.reset()
+
 
 agent = table_method.table_method()
+agent.train(env)
+observation = env.reset()
 for _ in range(1000):
     env.render()
-    action = env.action_space.sample()
+    # action = env.action_space.sample()
+    action = agent.get_action(observation)
     observation, reward, done, info = env.step(action)
     print("action:", action)
     print("obs:", agent.discrete_obs(observation))
