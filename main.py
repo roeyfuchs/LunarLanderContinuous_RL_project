99db728ab1841e97113d04e67587bf60050c5198
yuvalmor <yuvalmor94@gmail.com>
2021-03-12 09:32:45 +0200

    save

--- main.py
+++ main.py
@@ -10,5 +10,8 @@ if __name__ == '__main__':
 
     agent = SimpleDQN(env.observation_space.shape[0])
     rewards = agent.solve_env(env)
+
+    plt.xlabel("Number Episode")
+    plt.ylabel("Score Per Episode")
     plt.plot([i + 1 for i in range(0, len(rewards), 2)], rewards[::2])
     plt.show()
