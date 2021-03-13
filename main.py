c37525ba61ab21980778994080e8fc79f14a48fc
yuvalmor <yuvalmor94@gmail.com>
2021-03-13 08:55:04 +0200

    save

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from simple_dqn import SimpleDQN
 import gym
+import os
+import time
 import numpy as np
 import matplotlib.pyplot as plt
+
+
+BASE_SAVEING_PATH = "graphs"
+os.makedirs(
+    BASE_SAVEING_PATH, exist_ok=True
+)  # make sure that we have a directory to save
+FILE_SAVEING_TYPE = "png"
 
 
 if __name__ == '__main__':
@@ -12,14 +21,10 @@ if __name__ == '__main__':
     agent = SimpleDQN(env.observation_space.shape[0])
     rewards = agent.solve_env(env)
 
+    filename = time.strftime("%H-%M-%S", time.localtime())
+    file_path = os.path.join(BASE_SAVEING_PATH, filename)
     plt.xlabel("Number Episode")
     plt.ylabel("Score Per Episode")
     plt.plot([i + 1 for i in range(0, len(rewards), 2)], rewards[::2])
+    plt.savefig(".".join([file_path, FILE_SAVEING_TYPE]))
     plt.show()
-
-
-    def save(self, filename=None):
-        if not filename:
-            filename = time.strftime("%H-%M-%S", time.localtime())
-        file_path = os.path.join(BASE_SAVEING_PATH, filename)
-        self.fig.savefig(".".join([file_path, FILE_SAVEING_TYPE]))
