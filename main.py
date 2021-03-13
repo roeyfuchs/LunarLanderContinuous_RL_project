8fe99d878ca8bdb93023b9c1429cd8ead86cba52
yuvalmor <yuvalmor94@gmail.com>
2021-03-13 08:51:07 +0200

    save

--- main.py
+++ main.py
@@ -16,3 +16,10 @@ if __name__ == '__main__':
     plt.ylabel("Score Per Episode")
     plt.plot([i + 1 for i in range(0, len(rewards), 2)], rewards[::2])
     plt.show()
+
+
+    def save(self, filename=None):
+        if not filename:
+            filename = time.strftime("%H-%M-%S", time.localtime())
+        file_path = os.path.join(BASE_SAVEING_PATH, filename)
+        self.fig.savefig(".".join([file_path, FILE_SAVEING_TYPE]))
