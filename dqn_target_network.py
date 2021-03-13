403b639eff4cbb9db57ddbbf4aa420fa48593a88
yuvalmor <yuvalmor94@gmail.com>
2021-03-13 23:18:57 +0200

    Working PEX

--- dqn_target_network.py
+++ dqn_target_network.py
@@ -38,7 +38,7 @@ class DQNTargetNetwork:
     def select_action(self, state):
         if np.random.rand() <= self.epsilon:
             return random.randrange(self.action_space)
-        act_values = self.model.predict(state)
+        act_values = self.network.predict(state)
         return np.argmax(act_values[0])
 
     def replay(self):
@@ -55,13 +55,13 @@ class DQNTargetNetwork:
         states = np.squeeze(states)
         next_states = np.squeeze(next_states)
 
-        targets = rewards + self.gamma * (np.amax(self.model.predict_on_batch(next_states), axis=1)) * (1 - dones)
-        targets_full = self.model.predict_on_batch(states)
+        targets = rewards + self.gamma * (np.amax(self.target_network.predict_on_batch(next_states), axis=1)) * (1 - dones)
+        targets_full = self.network.predict_on_batch(states)
 
         ind = np.array([i for i in range(self.batch_size)])
         targets_full[[ind], [actions]] = targets
 
-        self.model.fit(states, targets_full, epochs=1, verbose=0)
+        self.network.fit(states, targets_full, epochs=1, verbose=0)
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
 
@@ -72,13 +72,20 @@ class DQNTargetNetwork:
             state = np.reshape(state, (1, 8))
             score = 0
             max_steps = 3000
+            update_target_model = utils.updateTargetNetwork
             for _ in range(max_steps):
                 action = self.select_action(state)
                 next_state, reward, done, _ = env.step(utils.discrete_actions[action])
                 score += reward
+                update_target_model -= 1
                 next_state = np.reshape(next_state, (1, 8))
                 self.save_state(state, action, reward, next_state, done)
                 state = next_state
+                if update_target_model == 0:
+                    print("yay")
+                    update_target_model = utils.updateTargetNetwork
+                    weights = self.network.get_weights()
+                    self.target_network.set_weights(weights)
                 self.replay()
                 if done:
                     print("episode: {}/{}, score: {}".format(episode, utils.episodes, score))
