0a1eee771ae322045865b885fa27d19220e8b67a
RoeyFuchs <fuchsroey@gmail.com>
2021-03-16 20:52:32 +0200

    now the main get arguments

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-from simple_dqn import SimpleDQN
-from queue import PriorityQueue
-from dqn_target_network import DQNTargetNetwork
-from dqn_prioritized_experience import DQNPrioritizedExperience
-from ddqn import DDQN
-import gym
+import argparse
 import os
+import sys
 import time
-import numpy as np
-import matplotlib.pyplot as plt
+from queue import PriorityQueue
 
+import gym
+import matplotlib.pyplot as plt
+import numpy as np
+
+from ddqn import DDQN
+from dqn_prioritized_experience import DQNPrioritizedExperience
+from dqn_target_network import DQNTargetNetwork
+from sarsa import SARSA
+from simple_dqn import SimpleDQN
 
 BASE_SAVING_PATH = "graphs"
 os.makedirs(
@@ -21,8 +25,52 @@ we based on the discrete code describe in:
 https://github.com/shivaverma/OpenAIGym/blob/master/lunar-lander/discrete/lunar_lander.py
 """
 
-if __name__ == '__main__':
+alogs_dict = {
+    "sarsa": SARSA,
+    "simple_dqn": SimpleDQN,
+    "ddqn": DDQN,
+    "dqn_p": DQNPrioritizedExperience,
+    "dqn_target": DQNTargetNetwork,
+}
+output = sys.stdout
+
+
+def set_verbose(v):
+    global output
+    if v:
+        output = sys.stdout
+    else:
+        output = open(os.devnull, "w")
+
+
+def printout(str):
+    global output
+    print(str, file=output)
+
+
+if __name__ == "__main__":
     env = gym.make("LunarLanderContinuous-v2")
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-v",
+        action="store_true",
+        help="verbose. if set will print out information and graph",
+    )
+    parser.add_argument(
+        "-r",
+        action="store_true",
+        help="Render. if set will render the lunar lander environment",
+    )
+    parser.add_argument(
+        "agent", choices=[i for i in alogs_dict], help="which agent to use"
+    )
+    args = parser.parse_args()
+
+    set_verbose(args.v)
+    print(alogs_dict[args.agent])
+    print(args)
+
     env.seed(0)
     np.random.seed(0)
 
