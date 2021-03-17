b786e5c79daa441e3ea31c1be4433b92b4d7ad6d
RoeyFuchs <fuchsroey@gmail.com>
2021-03-17 21:17:46 +0200

    last changes

--- main.py
+++ main.py
@@ -9,20 +9,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"  # hide debug msgs
+import utils
 from ddqn import DDQN
+from dqn_epsilon_greedy import DQNEpsilonGreedy
 from dqn_prioritized_experience import DQNPrioritizedExperience
 from dqn_target_network import DQNTargetNetwork
 from sarsa import SARSA
 from simple_dqn import SimpleDQN
 from uncertainty_env import uncertainty_env
-import utils
-
-BASE_SAVING_PATH = "graphs"
-os.makedirs(
-    BASE_SAVING_PATH, exist_ok=True
-)  # make sure that we have a directory to save
-FILE_SAVEING_TYPE = "png"
-
 
 alogs_dict = {
     "sarsa": SARSA,
@@ -30,7 +24,9 @@ alogs_dict = {
     "ddqn": DDQN,
     "dqn_p": DQNPrioritizedExperience,
     "dqn_target": DQNTargetNetwork,
+    "dqn_eg": DQNEpsilonGreedy,
 }
+
 
 def update_parser_options(parser):
     parser.add_argument(
@@ -54,7 +50,6 @@ def update_parser_options(parser):
     )
 
 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     update_parser_options(parser)
@@ -66,18 +61,13 @@ if __name__ == "__main__":
         env = gym.make("LunarLanderContinuous-v2")
 
     utils.set_verbose(args.v)
-    print(alogs_dict[args.agent])
-    print(args)
-
     env.seed(0)
     np.random.seed(0)
     agent = alogs_dict[args.agent](utils.printout, args.r)
     rewards = agent.solve_env(env)
 
-    filename = time.strftime("%H-%M-%S", time.localtime())
-    file_path = os.path.join(BASE_SAVING_PATH, filename)
-    plt.xlabel("Number Episode")
-    plt.ylabel("Score Per Episode")
-    plt.plot([i + 1 for i in range(0, len(rewards), 2)], rewards[::2])
-    plt.savefig(".".join([file_path, FILE_SAVEING_TYPE]))
-    plt.show()
+    if args.v:
+        plt.xlabel("Number Episode")
+        plt.ylabel("Score Per Episode")
+        plt.plot([i + 1 for i in range(0, len(rewards), 2)], rewards[::2])
+        plt.show()
--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ import os
 main_engine_values = [0, 0.5, 1]
 sec_engine_values = [-1, -0.75, 0, 0.75, 1]
 discrete_actions = [(x, y) for x in main_engine_values for y in sec_engine_values]
-episodes = 1000
+episodes = 800
 updateTargetNetwork = 50
 state_space = 8
 
